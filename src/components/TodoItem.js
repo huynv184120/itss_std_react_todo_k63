c98605ad075f4d26d98a917077976fb865cd8e9e
huynv184120 <nguyenviethuy2403@gmail.com>
2022-06-02 09:27:48 +0000

    create-new-component

---
+++ src/components/TodoItem.js
@@ -0,0 +1,16 @@
+/* 
+  【TodoItemコンポーネント】
+　・Todoアイテムを表示する
+　・チェックボックスにチェックが入っているか管理する
+　・チェックボックスにチェックが入っているかアイテムをグレーアウトする
+*/
+function TodoItem({item}) {
+  return (
+    <label className="panel-block">
+      <input type="checkbox" />
+      {item.text}
+    </label>
+  );
+}
+
+export default TodoItem;
